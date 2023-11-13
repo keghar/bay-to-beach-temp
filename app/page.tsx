be5c6bfc4b059a18fd17d7fb2ddab01fcc7b1064
keghar <108595664+keghar@users.noreply.github.com>
2023-11-13 15:06:29 -0600

    update content spacing and text size

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import logo from "./../public/baytobeach-logo.jpeg";
 
@@ -5,12 +6,17 @@ export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center bg-white">
-        <Image src={logo} alt="logo" height={300} />
-        <h1>Pool cleaning and maintenance in Gulf Shores, AL</h1>
-        <h2>Bay To Beach Pools LLC</h2>
-        <h2>Website comming soon</h2>
-        <p>Contact us at 251-233-5491 or baytobeachpoolsllc@gmail.com</p>
-        <p></p>
+        <Image className="pb-3" src={logo} alt="logo" height={300} />
+        <h2 className="text-3xl">Bay To Beach Pools LLC</h2>
+        <h1 className="text-2xl">
+          Pool cleaning and maintenance in Gulf Shores, AL
+        </h1>
+
+        <h2 className="pt-3">Website comming soon</h2>
+        <div className="flex flex-col justify-center items-center pt-3">
+          <p className="text-xl">Contact us:</p> <p>251-233-5491</p>
+          <p>baytobeachpoolsllc@gmail.com</p>
+        </div>
       </main>
     </>
   );

4be5d369d5e2971240aa2f487b20c98a7e546ded
keghar <108595664+keghar@users.noreply.github.com>
2023-11-13 15:22:25 -0600

    center text

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,21 @@ import logo from "./../public/baytobeach-logo.jpeg";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center bg-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-white">
         <Image className="pb-3" src={logo} alt="logo" height={300} />
-        <h2 className="text-3xl">Bay To Beach Pools LLC</h2>
-        <h1 className="text-2xl">
-          Pool cleaning and maintenance in Gulf Shores, AL
+        <h2 className="text-center text-xl md:text-3xl">
+          Bay To Beach Pools LLC
+        </h2>
+        <h1 className="text-lg md:text-2xl text-center">
+          Pool cleaning and maintenance in <br />
+          Gulf Shores, AL
         </h1>
 
-        <h2 className="pt-3">Website comming soon</h2>
+        <h2 className="text-center pt-3">Website coming soon</h2>
         <div className="flex flex-col justify-center items-center pt-3">
-          <p className="text-xl">Contact us:</p> <p>251-233-5491</p>
-          <p>baytobeachpoolsllc@gmail.com</p>
+          <p className="text-lg text-center md:text-xl">Contact us:</p>{" "}
+          <p className="text-center">251-233-5491</p>
+          <p className="text-center">baytobeachpoolsllc@gmail.com</p>
         </div>
       </main>
     </>
